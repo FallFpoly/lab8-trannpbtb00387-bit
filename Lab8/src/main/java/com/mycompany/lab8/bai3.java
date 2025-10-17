a332980e11e9bfe7aa93867ff26986f9a3a537b1
trannpbtb00387-bit <trannpbtb00387@fpt.edu.vn>
2025-10-17 23:14:19 +0700

    Lab7_bai3

--- Lab8/src/main/java/com/mycompany/lab8/bai3.java
+++ Lab8/src/main/java/com/mycompany/lab8/bai3.java
@@ -10,7 +10,31 @@ package com.mycompany.lab8;
  */
 public class bai3 {
 
+     public static final String toUpperFirstChar(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+
+        // Tach chuoi theo dau cach
+        String[] words = s.split(" ");
+
+        // Duyet tung tu trong mang
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() > 0) {
+                char firstChar = words[i].charAt(0);
+                String upperFirstChar = String.valueOf(firstChar).toUpperCase();
+                words[i] = upperFirstChar + words[i].substring(1);
+            }
+        }
+
+        // Noi cac tu lai thanh mot chuoi
+        String result = String.join(" ", words);
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String s = "nguyen van teo";
+        System.out.println("Chuoi ban dau: " + s);
+        System.out.println("Chuoi sau khi doi: " + toUpperFirstChar(s));
     }
 }
