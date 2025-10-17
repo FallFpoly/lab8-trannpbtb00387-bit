24384bf0f4a86c90c14dc5c9c8b3c85e48623b05
trannpbtb00387-bit <trannpbtb00387@fpt.edu.vn>
2025-10-17 23:14:12 +0700

    Lab7_bai2

--- Lab8/src/main/java/com/mycompany/lab8/bai2.java
+++ Lab8/src/main/java/com/mycompany/lab8/bai2.java
@@ -10,7 +10,35 @@ package com.mycompany.lab8;
  */
 public class bai2 {
 
+    public static final double min(double... x) {
+        if (x.length == 0) {
+            throw new IllegalArgumentException("Khong co phan tu nao!");
+        }
+        double min = x[0];
+        for (double i : x) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        return min;
+    }
+
+    // Phuong thuc tim so lon nhat
+    public static final double max(double... x) {
+        if (x.length == 0) {
+            throw new IllegalArgumentException("Khong co phan tu nao!");
+        }
+        double max = x[0];
+        for (double i : x) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("So nho nhat la: " + min(5, 2, 9, 1, 7));
+        System.out.println("So lon nhat la: " + max(5, 2, 9, 1, 7));
     }
 }
