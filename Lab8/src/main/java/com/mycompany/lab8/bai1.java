0916732601f55f540c091f3d7d50e9578743ecfc
trannpbtb00387-bit <trannpbtb00387@fpt.edu.vn>
2025-10-17 23:18:49 +0700

    Lab8_bai1

--- Lab8/src/main/java/com/mycompany/lab8/bai1.java
+++ Lab8/src/main/java/com/mycompany/lab8/bai1.java
@@ -9,7 +9,7 @@ package com.mycompany.lab8;
  * @author ICT
  */
 public class bai1 {
-
+        
      public static final double sum(double... x) {
         double tong = 0;
         for (double i : x) {
@@ -19,6 +19,7 @@ public class bai1 {
     }
 
     public static void main(String[] args) {
+        System.out.println("Nguyen Pham Bao Tran ");
         System.out.println("Tong 3 so 1.5, 2.5, 3.0 la: " + sum(1.5, 2.5, 3.0));
         System.out.println("Tong 5 so la: " + sum(1, 2, 3, 4, 5));
         System.out.println("Tong khong co phan tu la: " + sum());
