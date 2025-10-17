fa7c23fb5857bed14dca234f1b15a10b785ce05e
trannpbtb00387-bit <trannpbtb00387@fpt.edu.vn>
2025-10-17 23:14:26 +0700

    Lab7_bai4

--- Lab8/src/main/java/com/mycompany/lab8/bai4.java
+++ Lab8/src/main/java/com/mycompany/lab8/bai4.java
@@ -11,6 +11,12 @@ package com.mycompany.lab8;
 public class bai4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Tong: " + XPoly.sum(1, 2, 3, 4, 5));
+        System.out.println("So nho nhat: " + XPoly.min(5, 2, 9, 1, 7));
+        System.out.println("So lon nhat: " + XPoly.max(5, 2, 9, 1, 7));
+
+        String s = "nguyen van teo";
+        System.out.println("Chuoi ban dau: " + s);
+        System.out.println("Chuoi sau khi viet hoa: " + XPoly.toUpperFirstChar(s));
     }
 }
